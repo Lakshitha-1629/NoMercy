b2d63affc3e5db454bf715eafd8dd5de5616a1f7
Lakshitha-1629 <wijendraf5@gmail.com>
2025-12-27 18:11:40 +0530

    Update setting.js

--- setting.js
+++ setting.js
@@ -12,5 +12,5 @@ module.exports = {
     : process.env.SESSION_ID,
 
   PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-  SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+  SESSION_NAME: process.env.PORT === undefined ? "NoMercy" : process.env.SESSION_NAME,
 };
